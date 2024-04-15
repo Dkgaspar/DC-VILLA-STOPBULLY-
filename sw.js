d0821475f437a1db1f12f0f349de4d438e15ace4
Dkgaspar <145290570+Dkgaspar@users.noreply.github.com>
2024-04-15 07:03:52 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'bullying-quiz-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js',
+  // Agrega aquí todas las imágenes y archivos que necesitas cachear
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
